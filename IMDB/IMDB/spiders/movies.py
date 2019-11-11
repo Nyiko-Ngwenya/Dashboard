d0fcd6ec6cb443e75afb136e99c5bd62e89ca1b3
Nyiko <nyikongwenya@gmail.com>
2019-11-11 16:45:55 +0200

    Everything runs need to csv

--- IMDB/IMDB/spiders/movies.py
+++ IMDB/IMDB/spiders/movies.py
@@ -13,16 +13,16 @@ class Movies(scrapy.Spider):
                  'Runtime' : movies.xpath('.//p/span[@class="runtime"]/text()').extract_first(),
                  'Year' : movies.xpath('.//h3[@class="lister-item-header"]/span[@class = "lister-item-year text-muted unbold"]/text()').extract_first(),
                  'Synopsis' : movies.xpath('.//p[@class="text-muted"]/text()').extract_first(),
-                 'Votes' : movies.xpath('.//a[@class="lister-page-next next-page"]/@href').extract_first(),
-                 'Gross Amount' : movies.xpath('.//a[@class="lister-page-next next-page"]/@href').extract_first(),
-                 'Rating' : movies.xpath('.//a[@class="lister-page-next next-page"]/@href').extract_first(),
-                 'Metascore' : movies.xpath('.//a[@class="lister-page-next next-page"]/@href').extract_first()
+                 'Votes' : movies.xpath('.//p[@class="sort-num_votes-visible"]/span[2]/text()').extract_first(),
+                 'Gross Amount' : movies.xpath('.//p[@class="sort-num_votes-visible"]/span[5]/text()').extract_first(),
+                 'Rating' : movies.xpath('.//div[@class="inline-block ratings-imdb-rating"]/strong/text()').extract_first(),
+                 'Metascore' : movies.xpath('.//div[@class="inline-block ratings-metascore"]/span/text()').extract_first()
             }
 
-        # next_page = response.xpath('//a[@class="lister-page-next next-page"]/@href').extract_first()
-        # if next_page is not None:
-        #     next_page_link = response.urljoin(next_page)
-        #     yield scrapy.Request(url=next_page_link,callback=self.parse)
+        next_page = response.xpath('//a[@class="lister-page-next next-page"]/@href').extract_first()
+        if next_page is not None:
+            next_page_link = response.urljoin(next_page)
+            yield scrapy.Request(url=next_page_link,callback=self.parse)
             
            
 
