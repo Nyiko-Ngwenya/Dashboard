4627b9875f2741787e3838ed33027f11ac172b9a
Nyiko <nyikongwenya@gmail.com>
2019-11-11 16:23:16 +0200

    Got the right info to pull

--- IMDB/IMDB/spiders/keanu.py
+++ IMDB/IMDB/spiders/keanu.py
@@ -1,15 +1,28 @@
 import scrapy
-#from web_scraper.items import WebScraperItem
-#from scrapy.loader import ItemLoader
-class ScrapySpider(scrapy.Spider):
-	name = "websites"
-	start_urls = ['https://www.climatestotravel.com/climate/south-africa']
-	def parse(self,response):
-		for website in response.xpath("//div[@class='div-tabella']"):
-			print(website)
-			yield{
-                'movie_name': website
+
+class Movies(scrapy.Spider):
+    name = 'movies'
+    start_urls = ["https://www.imdb.com/search/title/?genres=comedy&genres=Romance&explore=title_type,genres&ref_=adv_explore_rhs"]
+
+
+    def parse(self,response):
+        for movies in response.xpath('//div[@class="lister-item-content"]'):
+            yield{
+                 'Movie_Name': movies.xpath('.//h3[@class="lister-item-header"]/a/text()').extract_first(),
+                 'Genre' : response.xpath('.//p[1]/span[@class="genre"]/text()').extract(),
+                 'Runtime' : response.xpath('.//p/span[@class="runtime"]/text()').extract_first(),
+                 'Year' : response.xpath('.//h3[@class="lister-item-header"]/span[@class="lister-item-year text-muted unbold"]/text()').extract_first(),
+                 'Synopsis' : response.xpath('.//p[@class="text-muted"]/text()').extract_first(),
+                 'Votes' : response.xpath('.//a[@class="lister-page-next next-page"]/@href').extract_first(),
+                 'Gross Amount' : response.xpath('.//a[@class="lister-page-next next-page"]/@href').extract_first(),
+                 'Rating' : response.xpath('.//a[@class="lister-page-next next-page"]/@href').extract_first(),
+                 'Metascore' : response.xpath('.//a[@class="lister-page-next next-page"]/@href').extract_first()
             }
-			#l = ItemLoader(item=WebScraperItem(), selector=website)
-			#l.add_xpath('website_tables',"./table")
-			#yield l.load_item()
+
+        # next_page = response.xpath('//a[@class="lister-page-next next-page"]/@href').extract_first()
+        # if next_page is not None:
+        #     next_page_link = response.urljoin(next_page)
+        #     yield scrapy.Request(url=next_page_link,callback=self.parse)
+            
+           
+
--- IMDB/IMDB/spiders/movies.py
+++ IMDB/IMDB/spiders/movies.py
@@ -3,29 +3,26 @@ import scrapy
 class Movies(scrapy.Spider):
     name = 'movies'
     start_urls = ["https://www.imdb.com/search/title/?genres=comedy&genres=Romance&explore=title_type,genres&ref_=adv_explore_rhs"]
-    # start_urls = ["https://www.laughfactory.com/jokes/family-jokes"]
+
 
     def parse(self,response):
         for movies in response.xpath('//div[@class="lister-item-content"]'):
-        # for movies in response.xpath('//a["lister-page next-page"]/text()'):
-            #print(movies)
             yield{
-                #  'movie_name': movies#.xpath('.//div[@class="joke-text"]/p').extract_first()
                  'Movie_Name': movies.xpath('.//h3[@class="lister-item-header"]/a/text()').extract_first(),
-                 'Genre' : response.xpath('//a[@class="lister-page-next next-page"]/@href').extract_first(),
-                 'Runtime' : response.xpath('//a[@class="lister-page-next next-page"]/@href').extract_first(),
-                 'Year' : response.xpath('//a[@class="lister-page-next next-page"]/@href').extract_first(),
-                 'Synopsis' : response.xpath('//a[@class="lister-page-next next-page"]/@href').extract_first(),
-                 'Votes' : response.xpath('//a[@class="lister-page-next next-page"]/@href').extract_first(),
-                 'Gross Amount' : response.xpath('//a[@class="lister-page-next next-page"]/@href').extract_first(),
-                 'Rating' : response.xpath('//a[@class="lister-page-next next-page"]/@href').extract_first(),
-                 'Metascore' : response.xpath('//a[@class="lister-page-next next-page"]/@href').extract_first(),
+                 'Genre' : movies.xpath('.//p[1]/span[@class="genre"]/text()').extract_first(),
+                 'Runtime' : movies.xpath('.//p/span[@class="runtime"]/text()').extract_first(),
+                 'Year' : movies.xpath('.//h3[@class="lister-item-header"]/span[@class = "lister-item-year text-muted unbold"]/text()').extract_first(),
+                 'Synopsis' : movies.xpath('.//p[@class="text-muted"]/text()').extract_first(),
+                 'Votes' : movies.xpath('.//a[@class="lister-page-next next-page"]/@href').extract_first(),
+                 'Gross Amount' : movies.xpath('.//a[@class="lister-page-next next-page"]/@href').extract_first(),
+                 'Rating' : movies.xpath('.//a[@class="lister-page-next next-page"]/@href').extract_first(),
+                 'Metascore' : movies.xpath('.//a[@class="lister-page-next next-page"]/@href').extract_first()
             }
 
-        next_page = response.xpath('//a[@class="lister-page-next next-page"]/@href').extract_first()
-        if next_page is not None:
-            next_page_link = response.urljoin(next_page)
-            yield scrapy.Request(url=next_page_link,callback=self.parse)
+        # next_page = response.xpath('//a[@class="lister-page-next next-page"]/@href').extract_first()
+        # if next_page is not None:
+        #     next_page_link = response.urljoin(next_page)
+        #     yield scrapy.Request(url=next_page_link,callback=self.parse)
             
            
 
